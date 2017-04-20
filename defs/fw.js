df958a289b4f74b11152c0b39fbac505b85a64bb
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2017-04-20 16:12:57 +0300

    fwEvents typings

--- defs/fw.js
+++ defs/fw.js
@@ -1,4 +1,36 @@
 // Unyson fw variable
 declare var fw:any;
-declare var fwEvents:any;
 
+declare type FwEventName = string;
+declare type FwEventMap = {
+	[eventName: FwEventName]: Function
+};
+
+declare type FwPrivateEventElement = {
+	context: ?Object,
+	listener: ?Function
+};
+
+declare type FwPrivateEventBox = {
+	[eventName: FwEventName]: FwPrivateEventElement[]
+};
+
+declare type FwEventMapOrName = FwEventName | FwEventMap;
+
+declare type FwEventPayload = mixed;
+
+declare type FwEventBox = {
+	log(message: string, data: FwEventPayload): void,
+	debug(enabled: boolean): FwEventBox,
+	on(message: FwEventMapOrName, callback: ?Function, context: ?Object): FwEventBox,
+	one(message: FwEventMapOrName, callback: ?Function, context: ?Object): FwEventBox,
+	off(message: FwEventMapOrName, callback: ?Function): FwEventBox,
+	trigger(message: FwEventMapOrName, data: FwEventPayload): FwEventBox,
+	hasListeners(message: FwEventName): boolean
+};
+
+declare var fwEvents: FwEventBox;
+
+declare module 'fw-events' {
+  declare var exports: FwEventBox;
+}
--- defs/other.js
+++ defs/other.js
@@ -2,3 +2,5 @@ declare module 'ct-log' {
   declare module.exports: any;
 }
 
+declare var __webpack_public_path__: any;
+
